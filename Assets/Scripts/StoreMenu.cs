a97bf2e16e5ca0c24e5923ade16d6fad25c192e5
Berk Cambaz <berkcambaz12321@gmail.com>
2020-07-31 21:53:02 +0300

    Added second page to store menu

---
+++ Assets/Scripts/StoreMenu.cs
@@ -0,0 +1,197 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StoreMenu : MonoBehaviour
+{
+    public StoreMenu instance;
+
+    private Touch touch;
+    private Vector3 touchPos;
+    private Vector3 touchPosOld;
+    private float scrollSensitivity = 2f;
+    private float move;
+
+    private Vector2 dragStartPos;
+    private bool isHeld = false;
+    private bool isMoved = false;
+    private int pageIndex = 0;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+#if RELEASE
+        // If received only 1 touch
+        if (Input.touchCount == 1)
+        {
+            touch = Input.GetTouch(0);
+            touchPos = touch.position;
+        }
+
+#else
+
+        if (Input.GetMouseButton(0))
+        {
+            touchPos = Input.mousePosition;
+        }
+#endif
+
+        DragAndDropMovement();
+        SlideControl();
+    }
+
+    void DragAndDropMovement()
+    {
+#if RELEASE
+        // If touch has started
+        if (touch.phase == TouchPhase.Began)
+        {
+            isHeld = true;
+
+            dragStartPos.x = touchPos.x;
+        }
+
+        // If touch has stopped
+        if (touch.phase == TouchPhase.Ended)
+        {
+            isHeld = false;
+            isMoved = false;
+        }
+
+#else
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isHeld = true;
+
+            dragStartPos.x = touchPos.x;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isHeld = false;
+            isMoved = false;
+        }
+#endif
+
+        // If touching
+        if (isHeld)
+        {
+            move = 0f;
+            if (touchPos.x > dragStartPos.x)
+                move = touchPos.x - dragStartPos.x;
+            else if (touchPos.x < dragStartPos.x)
+                move = touchPos.x - dragStartPos.x;
+
+            if (move != 0f)
+                isMoved = true;
+
+
+            if (!Mathf.Approximately(touchPos.x, touchPosOld.x))
+            {
+                touchPosOld.x = touchPos.x;
+                transform.localPosition = new Vector3(transform.localPosition.x + move * scrollSensitivity, 0f, 0f);
+            }
+            dragStartPos = touchPos;
+        }
+    }
+
+    void SlideControl()
+    {
+        pageIndex = Mathf.RoundToInt(transform.localPosition.x / -960f);
+
+        // Clamp the index
+        if (pageIndex < 0)
+            pageIndex = 0;
+        else if (pageIndex > 1)
+            pageIndex = 1;
+
+        if (!isHeld)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(pageIndex * -1920f, 0f, 0f), 0.1f);
+        }
+    }
+
+    public void InitUpgrades()
+    {
+        int upgradeCount = transform.GetChild(1).childCount;
+
+        for (int i = 0; i < upgradeCount; ++i)
+        {
+            
+        }
+    }
+
+    public void ButtonClick(int button)
+    {
+        UserData.isUnlockingCharacter = true;
+        switch (button)
+        {
+            case 0: // Buy with ad
+                    // TODO: Implement
+                break;
+            case 1: // Buy with money
+                    // If player has enough money & all skins are not unlocked
+                if (UserData.money > 99 && UserData.unlockedSkinCount != UserData.skinSprites.Length)
+                {
+                    UserData.money -= 100;
+                    UI.Update();
+                    // TODO: Animation for money going to the button
+                    StartCoroutine(BuyCharacter());
+                }
+                break;
+        }
+    }
+
+    private IEnumerator BuyCharacter()
+    {
+        // Start creating particles & shaking the camera
+        transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<ParticleSystem>().Play();
+        StartCoroutine(Utility.CameraShake(1f, 0.1f));
+
+        yield return new WaitForSeconds(1f);
+
+        // Shake the camera & create the newly unlocked character
+        StartCoroutine(Utility.CameraShake(0.25f, 0.5f));
+        UnlockRandomSkin();
+    }
+
+    private void UnlockRandomSkin()
+    {
+        int randSelectedSkin = Random.Range(0, UserData.skinSprites.Length);
+
+        // If character is already unlocked
+        if (UserData.skins[randSelectedSkin])
+        {
+            bool foundUnlockedSkin = false;
+            for (int i = randSelectedSkin; i < UserData.skinSprites.Length; ++i)
+                if (!UserData.skins[i])
+                {
+                    randSelectedSkin = i;
+                    foundUnlockedSkin = true;
+                }
+
+            // If haven't found the unlocked skin search for it
+            if (!foundUnlockedSkin)
+                for (int i = randSelectedSkin; i > UserData.skinSprites.Length; --i)
+                    if (!UserData.skins[i])
+                        randSelectedSkin = i;
+        }
+
+        // Unlock the character
+        UserData.skins[randSelectedSkin] = true;
+        UserData.lastOpenedSkin = randSelectedSkin; // Set the last opened skin to current opened skin
+
+        UI.notification.SetActive(true);
+
+        UserData.customizables[randSelectedSkin].transform.GetChild(0).GetChild(2).gameObject.SetActive(true);  // Activates skin's description
+        // Replace question mark image with skin's sprite
+        UserData.customizables[randSelectedSkin].transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = UserData.skinSprites[randSelectedSkin];
+
+        UserData.isUnlockingCharacter = false;
+    }
+}
